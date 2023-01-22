d052e64de27aa5f780b138264f0e028f9da5bcd2
Avinash Gaikwad <46967951+aviggithub@users.noreply.github.com>
2023-01-22 16:59:16 +0530

    Add files via upload

---
+++ Flask_docker_app/app_main.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from waitress import serve
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+     return 'This is a Python Flask Application for Test'
+
+if __name__ == '__main__':
+	serve(app, host="0.0.0.0", port=7779)
